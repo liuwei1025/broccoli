b93e2112cec788ad2f2dd3c8bb0c1b8d84792ae1
liuwei89 <we1025@qq.com>
2022-01-29 15:27:05 +0800

    feat: add server error case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import App from './App'
 
-test('renders request an invite', async () => {
+test('request an invite', async () => {
   render(<App />)
   const ele = screen.getByText(/Request an invite/i)
   expect(ele).toBeInTheDocument()
@@ -32,4 +32,38 @@ test('renders request an invite', async () => {
   fireEvent.click(send)
   await waitFor(() => Promise.resolve())
   expect(send.innerHTML).toEqual('Sending, please wait...')
+})
+
+test('handles server error', async () => {
+  render(<App />)
+
+  const ele = screen.getByText(/Request an invite/i)
+  fireEvent.click(ele)
+  await waitFor(() => screen.findByRole('dialog'))
+
+  // input
+  const name: HTMLInputElement = screen.getByTestId('name')
+  fireEvent.change(name, { target: { value: 'liuwei' } })
+  expect(name.value).toBe('liuwei')
+
+  const email: HTMLInputElement = screen.getByTestId('email')
+  fireEvent.change(email, { target: { value: 'usedemail@airwallex.com' } })
+
+  const confirm: HTMLInputElement = screen.getByTestId('confirm')
+  fireEvent.change(confirm, { target: { value: 'usedemail@airwallex.com' } })
+
+  expect(email.value).toEqual(confirm.value)
+
+  const send = screen.getByText(/Send/i)
+  expect(send).toBeInTheDocument()
+
+  // send
+  fireEvent.click(send)
+  await waitFor(() => screen.findByTestId('dialog-remote-error'), {
+    timeout: 1e4,
+  })
+
+  expect(screen.getByTestId('dialog-remote-error')).toHaveTextContent(
+    'Email is already in use'
+  )
 })
